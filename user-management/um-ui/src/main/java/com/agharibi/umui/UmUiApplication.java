922eb39794e71b60978bb828746a338d80333acd
Armen G <gharibi.armen@yahoo.com>
2018-12-28 15:03:50 -0800

    user management ui

---
+++ user-management/um-ui/src/main/java/com/agharibi/umui/UmUiApplication.java
@@ -0,0 +1,14 @@
+package com.agharibi.umui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmUiApplication.class, args);
+	}
+
+}
+
